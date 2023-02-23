e4855e91b83dd7ffbc9f42959b2fec195870ee60
sudeepmahato16 <sudeepmahato16@gmail.com>
2023-02-23 08:56:45 +0545

    About section completed

--- src/components/CustomTexts.tsx
+++ src/components/CustomTexts.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "./../utils/motion";
 
-export const TypingText = () => {};
+export const TypingText = ({
+  title,
+  textStyles,
+}: {
+  title: string;
+  textStyles: string;
+}) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => {
+      return (
+        <motion.span variants={textVariant2} key={index}>
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      );
+    })}
+  </motion.p>
+);
 
 export const TitleText = () => {};
